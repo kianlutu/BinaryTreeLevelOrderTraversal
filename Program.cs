1f261ff479110d9698e7e6c3e8490333e1fe4696
Kian Lutu <kianlutu@DESKTOP-GBAJR7R.redmond.corp.microsoft.com>
2019-08-10 11:29:31 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using BinaryTreeLevelOrderTraversal.Class;
+
+namespace BinaryTreeLevelOrderTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public static IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            IList<IList<int>> results = new List<IList<int>>();
+            results = Traversal(results,root,0);
+            return results;
+        }
+
+        public static IList<IList<int>> Traversal(IList<IList<int>> list, TreeNode root, int level)
+        {
+            if(root == null)
+            {
+                return list;
+            }
+
+            try
+            {
+                list[level].Add(root.val);
+            }
+            catch (System.Exception)
+            {
+                list.Add(new List<int>());
+                list[level].Add(root.val);
+                throw;
+            }
+   
+            
+            list = Traversal(list, root.left, level++);
+            list = Traversal(list, root.right, level++);
+            
+            return list;
+        }
+    }
+}
---
+++ class/TreeNode.cs
@@ -0,0 +1,14 @@
+namespace BinaryTreeLevelOrderTraversal.Class
+{
+    public class TreeNode
+    {
+        public int val { get; set; }
+        public TreeNode left { get; set; }
+
+        public TreeNode right { get; set; }
+        public TreeNode(int x) {
+            val = x;
+        }
+    }
+    
+}
